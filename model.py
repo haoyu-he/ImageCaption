38d5eb1dda941e7e0646dcc9e2927da4b4ea2430
Haoyu He <haoyuhe66@gmail.com>
2023-10-26 18:05:03 -0400

    update decoder

--- model.py
+++ model.py
@@ -3,14 +3,15 @@ import torch._utils
 import torch.nn as nn
 import torchvision.models as models
 
+from typing import Tuple
+
 
 class Encoder(nn.Module):
 
-    def __init__(self, img_emb_dim, device):
+    def __init__(self, img_emb_dim: int):
+        super().__init__()
 
-        super(Encoder, self).__init__()
         self.img_emb_dim = img_emb_dim
-        self.device = device
 
         # freeze encoder parameters
         encoder = models.resnet50(weights=models.ResNet50_Weights.DEFAULT)
@@ -26,8 +27,6 @@ class Encoder(nn.Module):
 
     def forward(self, images: torch.Tensor) -> torch.Tensor:
 
-        images = images.to(self.device)
-
         h = self.encoder(images)
         h = h.reshape(h.shape[0], -1)
         # h: (batch, 2048)
@@ -36,3 +35,45 @@ class Encoder(nn.Module):
         # h: (batch, img_emb_dim)
 
         return h
+
+
+class Decoder(nn.Module):
+
+    def __init__(self,
+                 img_emb_dim: int,
+                 word_emb_dim: int,
+                 hidden_dim: int,
+                 num_layers: int,
+                 vocab_size: int):
+        super().__init__()
+
+        self.img_emb_dim = img_emb_dim
+        self.word_emb_dim = word_emb_dim
+        self.hidden_dim = hidden_dim
+        self.num_layers = num_layers
+        self.vocab_size = vocab_size
+
+        self.decoder = nn.LSTM(input_size=self.img_emb_dim + self.word_emb_dim,
+                               hidden_size=self.hidden_dim,
+                               num_layers=self.num_layers)
+
+        self.fc = nn.Sequential(
+            nn.Linear(self.hidden_dim, self.vocab_size),
+            nn.LogSoftmax(dim=2)
+        )
+
+    def forward(self,
+                embedded_captions: torch.Tensor,
+                encoder_emb: torch.Tensor,
+                hidden: torch.Tensor,
+                cell: torch.Tensor) -> Tuple[torch.Tensor, Tuple[torch.Tensor, torch.Tensor]]:
+
+        decoder_input = torch.cat((embedded_captions, encoder_emb), dim=2)
+
+        decoder_output, (hidden, cell) = self.decoder(decoder_input, (hidden, cell))
+        # decoder_output: (length, batch, hidden_dim)
+
+        decoder_output = self.fc(decoder_output)
+        # decoder_output: (length, batch, vocab_size)
+
+        return decoder_output, (hidden, cell)
