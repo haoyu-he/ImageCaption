74345356246a702956821c8efe7ed88c70cbdeba
Haoyu He <haoyuhe66@gmail.com>
2023-10-26 03:53:17 -0400

    update config

--- config.py
+++ config.py
@@ -6,7 +6,6 @@ from dataclasses import dataclass
 
 @dataclass
 class Config:
-
     device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
 
     num_layers = 1
@@ -19,3 +18,21 @@ class Config:
     lr = 1e-3
 
     dataset_dir = os.path.join(os.path.dirname(os.path.realpath(__file__)), '..', 'dataset', 'Flick_30k')
+    encoder_file = ('src/encoder' +
+                    '_b' + str(batch) +
+                    '_h' + str(hidden_dim) +
+                    '_l' + str(num_layers) +
+                    '_e' + str(epoch) +
+                    '.pt')
+    decoder_file = ('src/decoder' +
+                    '_b' + str(batch) +
+                    '_h' + str(hidden_dim) +
+                    '_l' + str(num_layers) +
+                    '_e' + str(epoch) +
+                    '.pt')
+    embedding_file = ('src/embedding' +
+                      '_b' + str(batch) +
+                      '_h' + str(hidden_dim) +
+                      '_l' + str(num_layers) +
+                      '_e' + str(epoch) +
+                      '.pt')
